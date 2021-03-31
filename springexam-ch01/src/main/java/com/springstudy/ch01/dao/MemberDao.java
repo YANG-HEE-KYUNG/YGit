c654bdde914476b1b85ee70a401672365e08675e
gmlru <gmlru@192.168.0.37>
2021-03-31 12:10:55 +0900

    프로젝트 01번쨰 생성

--- springexam-ch01/src/main/java/com/springstudy/ch01/dao/MemberDao.java
+++
@@ -1,9 +0,0 @@
-package com.springstudy.ch01.dao;
-
-import java.util.ArrayList;
-
-import com.springstudy.ch01.domain.Member01;
-
-public interface MemberDao {
-	public ArrayList<Member01> getMemberList();
-}
--- springexam-ch01/src/main/java/com/springstudy/ch01/service/MemberService.java
+++
@@ -1,9 +0,0 @@
-package com.springstudy.ch01.service;
-
-import java.util.ArrayList;
-
-import com.springstudy.ch01.domain.Member01;
-
-public interface MemberService {
-	public ArrayList<Member01>getMemberList();
-}
